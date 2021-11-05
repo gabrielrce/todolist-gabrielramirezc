4a63d5075e7817617513b8ab85824974c234d4a3
gabrielrce <Gabriel.Ramirez@theksquaregroup.com>
2021-11-05 12:33:32 -0600

    pm2 add-on

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "app",
-      script: "./app.js",
+      script: "src/app.js",
       instances: "max",
       env: {
         NODE_ENV: "development",
