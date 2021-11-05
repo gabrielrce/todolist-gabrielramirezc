3d715047d242f7c5fc13af1580a9ce7f115bc902
gabrielrce <Gabriel.Ramirez@theksquaregroup.com>
2021-11-05 02:04:49 -0600

    adding env vars

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
 const db = require("./db/db");
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 3000;
 const todos = require("./routes/todos");
 
 app.use(express.json());
 app.use(todos);
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,7 @@
-const URI =
-  process.env.NODE_ENV === "production"
-    ? //production DB
-      process.env.DB_URL
-    : //dev DB
-      "mongodb+srv://dbMaster:Deathstroke1912@ormpracticedb.r9b4u.mongodb.net/todoDB?retryWrites=true&w=majority";
-
 const mongoose = require("mongoose");
-mongoose.connect(URI);
+require("dotenv").config("./variables.env");
+
+mongoose.connect(process.env.DB_URL);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Error al conectarse a la bdd"));
